b303ba3b9b222d24472c8a51d87591905381f0cb
jmillerLEARTH <99913522+jmillerLEARTH@users.noreply.github.com>
2022-05-19 15:06:42 -0500

    data entry

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ class database
 {
     constructor(){
         
-        this.data = [{name:"Joseph",birthday:"1989-06-24"},{name:"Adele",birthday:"1995-08-28"}];
+        this.data = [{name:"Elixir",Heals:10,Omega:4},{name:"Panacea",Heals:9},{name:"Silver Surfer",Heals:8},{name:"Angel",Heals:7},{name:"Iron Fist",Heals:6},{name:"Black Tarantula",Heals:5},{name:"Scarlet Witch",Heals:4},{name:"Mister Negative",Heals:3},{name:"Healer",Heals:2},{name:"Exodus",Heals:1,Omega:10},{name:"Beast",Wits:10,Charm:8},{name:"Prodigy",Wits:9},{name:"Charles Xavier",Wits:8,Charm:6},{name:"Dr. Moira MacTaggert",Wits:7},{name:"Mister Sinister",Wits:6},{name:"M",Wits:5},{name:"Kitty Pryde",Wits:4,Charm:10},{name:"Dr. Nemesis",Wits:3},{name:"Forge",Wits:2},{name:"Sage",Wits:1},{name:"Colossus",Charm:9},{name:"Storm",Charm:7,Bravery:9,Omega:9},{name:"Nightcrawler",Charm:5},{name:"Jean Grey",Charm:4,Bravery:6,Omega:8},{name:"Rogue",Charm:3},{name:"Wolverine",Charm:2,Bravery:10},{name:"Cyclops",Charm:1,Bravery:7},{name:"Magneto",Bravery:8,Omega:11},{name:"Mystique",Bravery:5},{name:"Sabretooth",Bravery:4},{name:"X23",Bravery:3},{name:"Rachel Summers",Bravery:2},{name:"Bishop",Bravery:1},{name:"Jamie Braddock",Omega:15},{name:"Mister M",Omega:14},{name:"Vulcan",Omega:13},{name:"Legion",Omega:12},{name:"Iceman",Omega:7},{name:"Proteus",Omega:6},{name:"Quentin Quire",Omega:5},{name:"Hope Summers",Omega:3},{name:"Nate Grey",Omega:2},{name:"Franklin Richards",Omega:1}];
     }
     
     OutputData(){
@@ -12,13 +12,37 @@ class database
         for(let i = 0; i < this.data.length; i++){
             
             let obj = this.data[i];
+//            
+//            $outputString += JSON.stringify(obj);
+//            
+            $outputString += "{";
             
-            $outputString += JSON.stringify(obj);
+            console.log(obj);
             
+            for(const key in obj){
+                
+                $outputString += key + ":";
+                
+                console.log($outputString);
+                
+                if(typeof obj[key] == "string"){
+                    
+                    $outputString += `"` + obj[key] + `"`;
+                }
+                else $outputString += obj[key];
+                
+                $outputString += ",";
+            }
+            
+            $outputString = $outputString.slice(0, -1);
+                
+            $outputString += "}";
+                
             if(i != this.data.length -1) $outputString += ",";
         }
         
         console.log($outputString);
+        console.table(this.data);
     }
     
     AddNewData(name,prop,value){
@@ -32,30 +56,27 @@ class database
         
         if($modObj == "undefined"){
             
-            let $tempObj = {name:"blank"};
+            $modObj = {name:name};
             
-            //$modObj = this.data.push($tempObj);
-            console.log($modObj);
-            $modObj.name = name;
+            this.data.push($modObj);
         }
         
-        $modObj.prop = prop;
-        $modObj.value = value;
+        $modObj[prop] = value;
     }
 }
 
 const db = new database()
 
-const $str = `Elixir
-Panacea
-Silver Surfer
-Angel
-Iron Fist
-Black Tarantula
-Scarlet Witch
-Mister Negative
-Healer
-Exodus`;
+const $str = `Charles Xavier
+Storm
+Cyclops
+Wolverine
+Jean Grey
+Kitty Pryde
+Magneto
+Cable
+Havok
+Emma Frost`;
 
 const $splitStr = $str.split(/\r?\n/);
 
@@ -63,7 +84,7 @@ for(let i = 0; i < $splitStr.length; i++){
     
     let $char = $splitStr[i];
     
-    db.AddNewData($char,"Healing Power",10-i);
+    db.AddNewData($char,"Leadership",$splitStr.length-i);
 }
 
 
