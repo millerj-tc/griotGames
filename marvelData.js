005299eb4fb788a9df8f0637a509ad17ca9616f7
Joseph Miller <j.sam.miller@gmail.com>
2022-05-29 21:29:30 -0500

    #22  marvel data added

---
+++ marvelData.js
@@ -0,0 +1,99 @@
+export const marvelData = [
+
+    {
+    name: "Beast",
+    dataType: "char",
+    power: "10",
+    cunning: "12",
+    charisma: "8",
+    cume: "30"
+    },
+    {
+    name: "Black Panther",
+    dataType: "char",
+    power: "9",
+    cunning: "7",
+    charisma: "12",
+    cume: "28"
+    },
+    {
+    name: "Bishop",
+    dataType: "char",
+    power: "6",
+    cunning: "11",
+    charisma: "7",
+    cume: "24"
+    },
+    {
+    name: "Cyclops",
+    dataType: "char",
+    power: "5",
+    cunning: "6",
+    charisma: "11",
+    cume: "22"
+    },
+    {
+    name: "Colossus",
+    dataType: "char",
+    power: "12",
+    cunning: "1",
+    charisma: "9",
+    cume: "22"
+    },
+    {
+    name: "Daredevil",
+    dataType: "char",
+    power: "1",
+    cunning: "10",
+    charisma: "10",
+    cume: "21"
+    },
+    {
+    name: "Jessica Jones",
+    dataType: "char",
+    power: "7",
+    cunning: "9",
+    charisma: "4",
+    cume: "20"
+    },
+    {
+    name: "Wolverine",
+    dataType: "char",
+    power: "11",
+    cunning: "4",
+    charisma: "3",
+    cume: "18"
+    },
+    {
+    name: "Okoye",
+    dataType: "char",
+    power: "2",
+    cunning: "5",
+    charisma: "9",
+    cume: "16"
+    },
+    {
+    name: "Deadpool",
+    dataType: "char",
+    power: "4",
+    cunning: "3",
+    charisma: "6",
+    cume: "13"
+    },
+    {
+    name: "Punisher",
+    dataType: "char",
+    power: "3",
+    cunning: "8",
+    charisma: "1",
+    cume: "12"
+    },
+    {
+    name: "Ghostrider",
+    dataType: "char",
+    power: "8",
+    cunning: "2",
+    charisma: "2",
+    cume: "12"
+    }
+]
