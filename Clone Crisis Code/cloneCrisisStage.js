e40e7d10c74065d7fa11b0a0045dac05c226a0eb
jmillerLEARTH <99913522+jmillerLEARTH@users.noreply.github.com>
2022-05-31 15:26:15 -0500

    cloneCrisis stage code

--- Clone Crisis Code/cloneCrisisStage.js
+++ Clone Crisis Code/cloneCrisisStage.js
@@ -6,6 +6,8 @@ export class cloneCrisisStage extends stage
         
         super(stageHandler);
         
+        this.uiHandler = this.stageHandler.scenarioHandler.gameHandler.uiHandler;
+        
         this.evalValue = "";
         this.displayText = "";
         this.winText = "";
@@ -15,7 +17,7 @@ export class cloneCrisisStage extends stage
         this.worstCharacterText = "[names] sucks at this";
         this.debug = false;
         this.evalDiv = document.createElement("div");
-        this.NPC;
+        this.NPC = null;
     }
 
     EvalFlow(){
@@ -45,22 +47,93 @@ export class cloneCrisisStage extends stage
         this.stageHandler.GotoNextStage(this.nextStage);
     }
     
-    _CharLastTeammateAtLoc(char){
-        
-        
-    }
-    
     _CloneCrisisBattle(eval){
         
-        const $highestCharismaChar = eval.pool.sort(function(a, b){return b.charisma- a.charisma});
+        this._ResetNPCRecruitmentProperties();
         
-        this.NPC.alignment = $highestCharismaChar.alignment;
+        this._NPCRecruitedByClosestCharisma(eval);
         
-        eval.pool.push(this.NPC);
+        this._NPCRecruitOutput();
+        
+        this._LowestCunningConfusedUnlessAlone(eval);
+        
+        this._LowestCunningConfusedOutput(eval);
+    }
+    
+    _NPCRecruitedByClosestCharisma(eval){
+        
+        if(this.NPC == null) return
+        
+        let $evalArr = [];
+        
+        let $diffArr = [];
+        
+        let $returnArr = [];
         
         for(const char of eval.pool){
             
+            let $charismaDiff = Math.abs(char.charisma - this.NPC.charisma);
             
+            $evalArr.push([char,$charismaDiff]);
+            
+            $diffArr.push($charismaDiff);
         }
+        
+        let $matchDiff = $diffArr.min();
+        
+        for(const i of $evalArr){
+            
+            if(i[1] == $matchDiff) $returnArr.push(i[0]);
+        }
+        
+        if($returnArr.length == 1){
+            
+            this.NPC.alignment = $returnArr[0].alignment;
+            
+            this.NPC.recruited = true;
+            
+            eval.pool.push(this.NPC);
+        }
+    }
+    
+    _ResetNPCRecruitmentProperties(){
+        
+        this.NPC.alignment = null;
+        this.NPC.recruited = false;
+    }
+    
+    _NPCRecruitOutput(){
+        
+        this.stageHandler.scenarioHandler.gameHandler.uiHandler.NewStageOutputDiv(this.NPC.name + " has decided to side with the " + this.NPC.alignment + " team.");
+    }
+    
+    _CharLastTeammateAtLoc(char){
+        
+        const $teammatesWithMyAlignment = char.location.GetCharsHere("any",char.alignment).length;
+        
+        if($teammatesWithMyAlignment > 1) return false
+        else if($teammatesWithMyAlignment == 1) return true
+        else console.warn("Error: _CharLastTeammateAtLoc() is malfunctioning");
+    }
+    
+    _LowestCunningConfusedUnlessAlone(eval){
+        
+        const $lowestCunningChar = eval.pool.sort(function(a, b){return a.cunning - b.cunning})[0];
+        
+        for(const char of eval.pool){
+            
+            if(this._CharLastTeammateAtLoc(char)) continue
+            
+            if(char == $lowestCunningChar){
+                
+                eval.pool.filter(c => c != $lowestCunningChar);
+                eval.confusedCharacter = char;
+            }
+        }
+    }
+    
+    _LowestCunningConfusedOutput(eval){
+        
+        if(eval.confusedCharacter != null) this.uiHandler.NewStageOutputDiv(eval.confusedCharacter.name + " imperfectly execute their team plan, they are out of position!");
     }
 }
